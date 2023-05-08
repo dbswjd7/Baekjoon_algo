0a795a5c9ffb51d97d301ebe57892020f9ab76ef
yunjeong <yj2171063@ewhain.net>
2023-05-08 22:59:47 +0900

    step1

---
+++ 18108.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin>> A;
+    B = A-543;
+    cout<<B;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/BaekJoon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BaekJoon"
+  "BaekJoon.pdb"
+  "CMakeFiles/BaekJoon.dir/10172.cpp.o"
+  "CMakeFiles/BaekJoon.dir/10172.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BaekJoon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
