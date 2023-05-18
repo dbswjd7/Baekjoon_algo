3eef1cdd804186910f4ec0b12d6c8fca9a71d9e2
yunjeong <yj2171063@ewhain.net>
2023-05-18 21:51:30 +0900

    step2(2)

--- 2480.cpp
+++ 2480.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <algorithm>
 using namespace  std;
 
-void printPrize(){
+void printPrize(int a, int b, int c){
+    int p;
+    if((a==b)&&(b==c)&&(a==c)) p=10000+1000*a;
+    else if((a!=b)&&(b!=c)&&(a!=c)){
+        p=100*max({a,b,c});
+    }else {
+        if((a==b)||(a==c)) p=1000+100*a;
+        else p=1000+100*b;
+    }
+    cout<<p;
 
 }
 
 int main(){
     int a,b,c;
     cin>>a>> b>> c;
+    printPrize(a,b,c);
     return 0;
 }
